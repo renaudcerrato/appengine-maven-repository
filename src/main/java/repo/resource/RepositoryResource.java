e424dd6ed00049233393cf2333f4ddb90fa0fd59
Anthony Chuinard <tony@twansoftware.com>
2019-11-13 17:43:33 -0600

    Add handler for _ah/start
    Required for App Engine

--- src/main/java/repo/resource/RepositoryResource.java
+++ src/main/java/repo/resource/RepositoryResource.java
@@ -37,6 +37,12 @@ public class RepositoryResource {
 
     private final GcsService gcs = GcsServiceFactory.createGcsService();
     private final BlobstoreService blobstore = BlobstoreServiceFactory.getBlobstoreService();
+    
+    @GET
+    @Path("/_ah/start")
+    public Boolean startup() {
+        return Boolean.TRUE;
+    }
 
     @GET
     @Template(name = "/list.mustache")
